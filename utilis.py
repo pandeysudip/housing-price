99e0fedbd538c9a6d80fd27c346ba82ad153a2cf
pandeysudip <sudip@siu.edu>
2021-12-25 17:49:45 -0800

    updating model function

--- utilis.py
+++ utilis.py
@@ -1,12 +1,14 @@
 import joblib
 import numpy as np
+from sklearn.preprocessing import StandardScaler
 
 
-def preprocess(variables=['City', 'Monthly Owner Cost', 'Per Capita Income',
+def preprocess(variables=['Monthly Owner Cost', 'Per Capita Income',
                           'Lng', 'Household Income', 'College Rate', 'Lat',
                           'Personal Transport Rate', 'High School Rate', 'Median Age',
                           'Public Transport Rate', 'Population']):
-    test_data = np.array(variables)
+    std_scaler = StandardScaler()
+    test_data = std_scaler.fit_transform([variables])
     trained_model = joblib.load('model.pkl')
     prediction = trained_model.predict(test_data)
 
